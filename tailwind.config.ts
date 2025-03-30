62216db60d7d1a7d033e164441d8cb80cae98903
bhardwajakshit <bhardwajakshit78@gmail.com>
2025-03-31 00:59:17 +0530

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/core/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      keyframes: {
+        pulseGlow: {
+          '0%, 100%': { boxShadow: '0 0 6px #b0633a' },
+          '50%': { boxShadow: '0 0 20px #b0633a' },
+        },
+      },
+      animation: {
+        spin: 'spin 5s linear infinite both',
+        pulseGlow: 'pulseGlow 2s infinite ease-in-out',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'code-background': '#1e1e1e',
+      },
+      fontSize: {
+        md: '14px',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
